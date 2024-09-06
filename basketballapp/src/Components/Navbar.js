f2854381a3945d9e004459ef1810f06c4dfeec34
Lui-VEVO <122450740+Lui-VEVO@users.noreply.github.com>
2024-09-06 12:20:56 +0200

    Fixed styles for navbar

--- basketballapp/src/Components/Navbar.js
+++ basketballapp/src/Components/Navbar.js
@@ -3,15 +3,20 @@ import "../styles.css";
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <div>
-        <ul className="navItems">
-          <li>Basketball shoes</li>
-          <li>About us</li>
-          <li>Contact us</li>
-        </ul>
-      </div>
-      <div className="cart-icon"></div>
-    </nav>
+    <div className="navbar-container">
+      <nav className="upper-navbar">
+        <span>Ballerz</span>
+      </nav>
+      <nav className="lower-navbar">
+        <div>
+          <ul className="navItems">
+            <li>Basketball shoes</li>
+            <li>About us</li>
+            <li>Contact us</li>
+          </ul>
+        </div>
+        <div className="cart-icon"></div>
+      </nav>
+    </div>
   );
 }
